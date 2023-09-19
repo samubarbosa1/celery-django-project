a2eb1f109ed5fe543a6aba697f87dcb9ba78eaaf
Barbosa <samuel.silva@ime.eb.br>
2023-09-19 17:01:22 -0300

    mudança nas tasks

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -5,8 +5,13 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcelery.settings")
 app = Celery("dcelery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.conf.task_routes = {
-    "newapp.tasks.task1": {"queue": "queue1"},
-    "newapp.tasks.task2": {"queue": "queue2"},
+# app.conf.task_routes = {
+#     "newapp.tasks.task1": {"queue": "queue1"},
+#     "newapp.tasks.task2": {"queue": "queue2"},
+# }
+app.conf.broker_transport_options = {
+    'priority_steps': list(range(10)),
+    'sep':':',
+    'queue_order_strategy':'priority',
 }
 app.autodiscover_tasks()
--- dcelery/newapp/tasks.py
+++ dcelery/newapp/tasks.py
@@ -1,11 +1,26 @@
 from celery import shared_task
+import time
 
 
 @shared_task
-def task1():
+def tp1():
+    time.sleep(3)
     return
 
 
 @shared_task
-def task2():
+def tp2():
+    time.sleep(3)
+    return
+
+
+@shared_task
+def tp3():
+    time.sleep(3)
+    return
+
+
+@shared_task
+def tp4():
+    time.sleep(3)
     return
